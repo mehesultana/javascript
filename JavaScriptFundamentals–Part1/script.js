1f7b20c6e0db1e4cbcf8bb1e0b553f199621229c
mehesultana <sultanamehe01@gmail.com>
2022-07-23 21:42:05 +0600

    Data Types

--- JavaScriptFundamentals–Part1/script.js
+++ JavaScriptFundamentals–Part1/script.js
@@ -12,3 +12,19 @@ let myCurrentJob = 'Student';
 let job1 = 'programmer';
 let job2 = 'student';
 //console.log(myFirstJob);
+
+//  ----- Data Types -----
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+javascriptIsFun = 'YES';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 2000;
+console.log(typeof year);
