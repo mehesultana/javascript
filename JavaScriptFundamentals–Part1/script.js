9e98944247bdd5344eeea524540cb0dfefd2d982
mehesultana <sultanamehe01@gmail.com>
2022-07-23 21:04:23 +0600

    values and variable

---
+++ JavaScriptFundamentals–Part1/script.js
@@ -0,0 +1,14 @@
+// ------ values & variable ------
+
+let firstName = 'Mehe';
+//console.log(firstName);
+
+let mehe_sultana = 'MS';
+let $function = 35;
+let person = 'Noman';
+let PI = 3.1415;
+let myFirstJob = 'Coder';
+let myCurrentJob = 'Student';
+let job1 = 'programmer';
+let job2 = 'student';
+//console.log(myFirstJob);
