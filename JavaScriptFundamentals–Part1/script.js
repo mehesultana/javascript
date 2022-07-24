fad06ff1d99e265d070ddf3667005443e113976e
mehesultana <sultanamehe01@gmail.com>
2022-07-24 21:49:29 +0600

    Basic Operators

--- JavaScriptFundamentals–Part1/script.js
+++ JavaScriptFundamentals–Part1/script.js
@@ -38,5 +38,28 @@ const birthYear = 2000;
 // const job;
 var job = 'programmer';
 job = 'student';
-lastName = 'Schmedtmann';
+lastName = 'SM';
 console.log(lastName);
+
+// Basic Operators
+
+//------ Math operators------
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+const name1 = 'Mehe';
+const name2 = 'SUltana';
+console.log(name1 + ' ' + name2);
+
+// Assignment operators
+
+let x = 30 + 6;
+x += 20;
+x *= 7;
+x++;
+x--;
+
+console.log(x);
